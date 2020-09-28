40aebf97771117ff7cb9381877f47cffebadd50d
thuuor <thuuor@gmail.com>
2020-09-28 19:07:18 +0200

    Add New type of letter Add Toolbar in all layouts Add Styles and Strings

--- app/src/main/java/com/example/playjuego/MainActivity.kt
+++ app/src/main/java/com/example/playjuego/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.playjuego
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +19,28 @@ class MainActivity : AppCompatActivity() {
         player.setOnClickListener {
             lanzarNewPlayer()
         }
+
+        val titulo = findViewById(R.id.titulo) as TextView
+        titulo.setTypeface(Typeface.createFromAsset(assets, "Courgette-Regular.ttf"))
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 
     fun lanzarNewPlayer(){
         val i = Intent(this,Newplayer::class.java)
         startActivity(i)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {  (R.id.action_search) -> {return true}
+            (R.id.action_add) -> {return true}
+            else -> {return super.onOptionsItemSelected(item)}
+        }
     }
 }

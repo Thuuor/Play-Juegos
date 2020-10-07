e7f7a8e8445a2cebbee39fd1842a548b421dc6b9
thuuor <thuuor@gmail.com>
2020-10-07 19:21:58 +0200

    Change Activity_main buttons and added chipgroup on Generos

--- app/src/main/java/com/example/playjuego/Generos.kt
+++ app/src/main/java/com/example/playjuego/Generos.kt
@@ -2,12 +2,20 @@ package com.example.playjuego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
 class Generos : AppCompatActivity() {
+    var option : String = "No has elegido ninguna opcion"
+    var checked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.generos)
@@ -16,8 +24,26 @@ class Generos : AppCompatActivity() {
         bottomAppBar.replaceMenu(R.menu.menu_main)
 
         val fab : FloatingActionButton = findViewById(R.id.fab)
+        val chipGroup = findViewById(R.id.chipGroup) as ChipGroup
 
         fab.setOnClickListener({Snackbar.make(it,"Movimiento del boton",Snackbar.LENGTH_LONG).show()})
 
+        chipGroup.setOnCheckedChangeListener(object : ChipGroup.OnCheckedChangeListener {
+
+            override fun onCheckedChanged(group: ChipGroup, checkedId: Int) {
+                val checkedChip = group.findViewById<Chip>(checkedId)
+                option = checkedChip.text as String
+                checked = true
+            }
+        })
+
+        fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (checked) {
+                    Toast.makeText(getApplicationContext(), option, Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(getApplicationContext(), option, Toast.LENGTH_LONG).show();
+            }
+        })
     }
 }
--- app/src/main/java/com/example/playjuego/Preferences.kt
+++ app/src/main/java/com/example/playjuego/Preferences.kt
@@ -2,20 +2,26 @@ package com.example.playjuego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.RatingBar
-import android.widget.SeekBar
+import android.view.View
+import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Preferences : AppCompatActivity() {
+
+    var option : String = "No has pulsado ninguna opcion"
+    var score:kotlin.String? = ""
+    var checked = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.preferences)
 
         val sb = findViewById(R.id.seekBar1) as SeekBar
         val rb = findViewById(R.id.ratingBar1) as RatingBar
+        val fabpref : FloatingActionButton = findViewById(R.id.fabpref)
+        val rGroup = findViewById(R.id.radioGroup) as RadioGroup
 
-        rb.onRatingBarChangeListener = RatingBar.OnRatingBarChangeListener{ ratingBar, rating, fromUser ->
-            sb.progress = rating.toInt()
-        }
 
         sb.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -30,6 +36,35 @@ class Preferences : AppCompatActivity() {
 
             }
         })
+
+        rGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+
+            override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
+                val checkedRadioButton = rGroup.findViewById<RadioButton>(checkedId)
+                option = checkedRadioButton.text as String
+                checked = true
+            }
+        })
+
+        rb.onRatingBarChangeListener =
+            RatingBar.OnRatingBarChangeListener{
+                ratingBar, rating, fromUser ->  sb.progress = rating.toInt()
+            }
+
+
+        fabpref.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if (checked){
+                    score = " La puntuacion que le has dado al juego " + option + " es de " + sb.progress
+                    Toast.makeText(getApplicationContext(), score , Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), option , Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        })
+
 
     }
 
